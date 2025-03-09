2be1d5cb345108840c436c38827a3a27de888f9a
netford <konstpon2012@gmail.com>
2025-03-09 17:21:26 +0300

    "Увеличить контрастность элементов фильтрации в разделе 'Наши работы'
    Former-commit-id: 1d2b1938ec6e130527410e5f3a794babe21ce999

--- src/components/ReadySuits.jsx
+++ src/components/ReadySuits.jsx
@@ -1,6 +1,6 @@
 // ReadySuits.jsx
 import React, { useState, useRef, useEffect, useMemo } from 'react';
-import { ShoppingBag, MessageCircle, Phone } from 'lucide-react';
+import { ShoppingBag, MessageCircle, Phone, Filter } from 'lucide-react';
 import ProductImageSlider from './ProductImageSlider';
 import SuitCard from './SuitCard';
 import styles from './ReadySuits.module.css';
@@ -685,50 +685,53 @@ return (
 </div>
 
 <div className={styles.filtersContainer}>
-  <select
-    className={styles.filterSelect}
-    value={activeFilter}
-    onChange={handleFilterChange}
-    disabled={isTransitioning}
-  >
-    <option value="all" className={styles.mainOption}>
-      Все купальники ({getCountForFilter('all')})
-    </option>
-    <optgroup label="Категории">
-      <option value="acrobatics_gymnastics">
-        Акробатика/Гимнастика ({getCountForFilter('acrobatics_gymnastics')})
+  <div className={styles.filterWrapper}>
+    <Filter className={styles.filterIcon} size={20} />
+    <select
+      className={styles.filterSelect}
+      value={activeFilter}
+      onChange={handleFilterChange}
+      disabled={isTransitioning}
+    >
+      <option value="all" className={styles.mainOption}>
+        Все купальники ({getCountForFilter('all')})
       </option>
-      <option value="figure-skating">
-        Фигурное катание ({getCountForFilter('figure-skating')})
+      <optgroup label="Категории">
+        <option value="acrobatics_gymnastics">
+          Акробатика/Гимнастика ({getCountForFilter('acrobatics_gymnastics')})
+        </option>
+        <option value="figure-skating">
+          Фигурное катание ({getCountForFilter('figure-skating')})
+        </option>
+        <option value="renta">
+          Прокат ({getCountForFilter('renta')})
+        </option>
+      </optgroup>
+      <optgroup label="В наличии">
+        <option value="available">
+          Все размеры ({getCountForFilter('available')})
+        </option>
+        <option value="available-124">
+          до 124 см. ({getCountForFilter('available-124')})
+        </option>
+        <option value="available-129">
+          125-129 см. ({getCountForFilter('available-129')})
+        </option>
+        <option value="available-139">
+          130-139 см. ({getCountForFilter('available-139')})
+        </option>
+        <option value="available-154">
+          140-154 см. ({getCountForFilter('available-154')})
+        </option>
+        <option value="available-155">
+          от 155 см. ({getCountForFilter('available-155')})
+        </option>
+      </optgroup>
+      <option value="sold">
+        Продано ({getCountForFilter('sold')})
       </option>
-      <option value="renta">
-        Прокат ({getCountForFilter('renta')})
-      </option>
-    </optgroup>
-    <optgroup label="В наличии">
-      <option value="available">
-        Все размеры ({getCountForFilter('available')})
-      </option>
-      <option value="available-124">
-        до 124 см. ({getCountForFilter('available-124')})
-      </option>
-      <option value="available-129">
-        125-129 см. ({getCountForFilter('available-129')})
-      </option>
-      <option value="available-139">
-        130-139 см. ({getCountForFilter('available-139')})
-      </option>
-      <option value="available-154">
-        140-154 см. ({getCountForFilter('available-154')})
-      </option>
-      <option value="available-155">
-        от 155 см. ({getCountForFilter('available-155')})
-      </option>
-    </optgroup>
-    <option value="sold">
-      Продано ({getCountForFilter('sold')})
-    </option>
-  </select>
+    </select>
+  </div>
 </div>
 
 <div 
